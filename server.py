68adfab951d674328ea4b8bd1dee36c01dd9add1
ayushxx7 <ayushxx7@gmail.com>
2018-12-29 06:13:49 +0530

    before db stage

--- server.py
+++ server.py
@@ -1,16 +1,48 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretKeyLol'
+
+
+class UserLogin(FlaskForm):
+	username = StringField('username', validators = [InputRequired(), Length(min =4, max=30)])
+	password = PasswordField('password', validators= [InputRequired(), Length(min = 5, max = 80)])
+	remember = BooleanField('Remember Me?')
+
+class Signup(FlaskForm):
+	username = StringField('username', validators = [InputRequired(), Length(min =4, max=30)])
+	password = PasswordField('password', validators= [InputRequired(), Length(min = 5, max = 80)])
 
 @app.route('/')
 def index():
+	
 	return(render_template('index.html'))
 
-@app.route('/userLogin')
+@app.route('/userLogin', methods = ['POST','GET'])
 def userLogin():
-	return(render_template('userLogin.html'))
+	form = UserLogin()
 
-@app.route('/agentLogin')
+	if form.validate_on_submit():
+
+		return render_template('services.html')
+
+
+	return(render_template('userLogin.html', form=form))
+
+@app.route('/signup', methods = ['POST','GET'])
+def signup():
+	form = Signup()
+
+	if form.validate_on_submit():
+		return render_template('services.html')
+
+	return(render_template('signup.html', form=form))
+
+@app.route('/agentLogin', methods = ['POST','GET'])
 def agentLogin():
 	return(render_template('userLogin.html'))
 
